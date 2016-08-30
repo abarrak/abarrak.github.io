16780bfdc42fa2e1776e38687914f7e7af124ee5
abarrak <a@abarrak.com>
2016-08-30 09:19:19 +0300

    Source Auto Update at: Tue Aug 30 09:19:19 AST 2016

---
+++ public/all.min.js
@@ -0,0 +1,1 @@
+!function(e){var c=e.querySelector(".sidebar-toggle"),r=e.querySelector("#sidebar"),t=e.querySelector("#sidebar-checkbox");e.addEventListener("click",function(e){var n=e.target;t.checked&&!r.contains(n)&&n!==t&&n!==c&&(t.checked=!1)},!1)}(document);
