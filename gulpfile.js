05190f71bbffb282766bca4aa4e272c48067c492
Abdullah Barrak <a@abarrak.com>
2021-07-11 18:55:35 +0300

    fix publish flow and cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ function styles(_cb) {
     .pipe(concatCss("public/css/all.css"))
     .pipe(rename('all.min.css'))
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(dest('../abarrak.github.io/public/css'));
+    .pipe(dest('public/css'));
   _cb();
 }
 
@@ -20,7 +20,7 @@ function scripts(_cb) {
     .pipe(concat('all.js'))
     .pipe(rename('all.min.js'))
     .pipe(uglify())
-    .pipe(dest("../abarrak.github.io/public/js"));
+    .pipe(dest("public/js"));
   _cb();
 }
 
